52ea98be785e39ac3f5528e1f3acd957708e4d7c
Tanusha98 <42642389+Tanusha98@users.noreply.github.com>
2020-10-27 07:05:19 +0530

    Add files via upload

--- geo-spatial-demo/src/main/java/com/freelance/geospatial/demo/configuration/GeoConfiguration.java
+++ geo-spatial-demo/src/main/java/com/freelance/geospatial/demo/configuration/GeoConfiguration.java
@@ -1,12 +1,22 @@
 package com.freelance.geospatial.demo.configuration;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.http.codec.support.DefaultServerCodecConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.slf4j.Logger;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -15,11 +25,45 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import static springfox.documentation.builders.PathSelectors.regex;
 
+import javax.annotation.PostConstruct;
+
 @EnableAutoConfiguration
 @ComponentScan
 @EnableSwagger2
 @SpringBootConfiguration
 public class GeoConfiguration{
+	
+
+	private static final Logger log = LoggerFactory.getLogger(GeoConfiguration.class);
+	
+	@Value("${spring.data.mongodb.host}")
+    private String mongoHost;        	
+
+    @Autowired
+    private MongoMappingContext mongoMappingContext;
+
+    @PostConstruct
+    public void init() {
+    	this.mongoMappingContext.setAutoIndexCreation(true);
+    	log.info("Mongo AutoIndexCreation: {}", this.mongoMappingContext.isAutoIndexCreation());
+    }
+    
+    @Bean
+    public MongoClient reactiveMongoClient()  {
+    	String myHost = System.getenv("MONGODB_HOST");		
+		log.info("MONGODB_HOST="+myHost);
+        if(myHost==null) {
+        	return MongoClients.create();
+        }
+		return MongoClients.create("mongodb://"+ myHost);
+    }	
+
+    @Bean
+	public ServerCodecConfigurer serverCodecConfigurer() {
+		return new DefaultServerCodecConfigurer();
+	}
+    
+
 	@Bean
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
@@ -28,5 +72,5 @@ public class GeoConfiguration{
           .paths(regex("/geodata.*"))                          
           .build();                                           
     }
-	
+
 }
